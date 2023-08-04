d289330312eacaef406520efedcb72ee06ed59f4
Mike Camenzuli <36173249+VonZuli@users.noreply.github.com>
2023-08-04 14:36:51 -0400

    Changed header image, created password validation with regex, created initial email validation logic

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
 "use strict";
 
-
 const submitBtn = document.querySelector("#footer-section > button");
-
 const checkPass1 = document.querySelector("#password");
 const checkPass2 = document.querySelector("#confirm_password");
-
+const email = document.querySelector("#email");
+const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+const emailPattern =
+  /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g;
 
 submitBtn.addEventListener("click", validatePassword);
-function validatePassword() {
-  // console.log('TEST');
-  if (checkPass1.value !=  checkPass2.value) {
-    //add a class of "match" to input variables
+submitBtn.addEventListener("click", validateEmail);
 
+function validatePassword() {
+  if (checkPass1.value != checkPass2.value) {
+    //add a class of "error" to input variables if passwords do not match each other
     checkPass1.classList.add("error");
     checkPass2.classList.add("error");
-
-    // input.style.border = "2px solid green";
-    // input.style.border = "2px solid green";
+    console.log("no match");
+  } else if (checkPass1.value === "" || checkPass2.value === "") {
+    //add a class of "error" to input variables if password fields are blank
+    checkPass1.classList.add("error");
+    checkPass2.classList.add("error");
+    console.log("blank");
+  } else if (checkPass1.value.length < 8 || checkPass1.value.length > 15) {
+    //add a class of "error" to input variables if password fields do not meet length requirements
+    checkPass1.classList.add("error");
+    checkPass2.classList.add("error");
+    console.log("length");
+  } else if (!checkPass1.value.match(passwordPattern)) {
+    //add a class of "error" to input variables if password fields do not match regex pattern
+    checkPass1.classList.add("error");
+    checkPass2.classList.add("error");
+    console.log("pattern");
   } else {
-    //add a class of "error" to input variables
-    checkPass1.classList.add("match");
-    checkPass2.classList.add("match");
+    //add a class of "match" to input variables
+    checkPass1.classList.remove("error");
+    checkPass2.classList.remove("error");
+    checkPass1.classList.add("valid");
+    checkPass2.classList.add("valid");
+    console.log("valid");
   }
 }
 
-// input.addEventListener("focus", ()=>{
-//   input.style.border = "2px solid #e5e7eb";
-// })
+// function validateEmail() {
+//   if (email.value.match(emailPattern)) {
+//     email.classList.add("valid")
+//   } else{
+//     email.classList.add("error")
+//     console.log("email pattern");
+//   }
+// }
